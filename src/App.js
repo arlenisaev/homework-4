0ed15af97d3dc11c078d4e9d347df7b0821b517f
arlenisaev <arlenisaev43@gmail.com>
2023-05-26 13:12:17 +0600

    homework4

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      phone: '',
+      email: '',
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('ФИО:', this.state.name);
+    console.log('Номер телефона:', this.state.phone);
+    console.log('Email адрес:', this.state.email);
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className='box'>
+        <div className='block'>
+          <div className='block2'>
+            <h3>Регистрация пользователя</h3>            
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div className='n-phone'>
+              <label htmlFor="name">ФИО:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            
+            <div className='n-phone'>
+              <label htmlFor="phone">Номер телефона:</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className='n-phone'>
+              <label htmlFor="email">Email адрес:</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RegistrationForm;
+
